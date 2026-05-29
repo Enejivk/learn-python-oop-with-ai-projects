222ed5baf48edf800d13e96255fcaa93084ef28c
enejivk <enejivic@gmail.com>
2026-05-29 03:48:29 +0100

    done with loop

---
+++ section-02-python-fundamentals/code/loop.py
@@ -0,0 +1,36 @@
+# LOOPS
+# 1. for loop
+# 2. while loop
+
+# for i in range(2):
+#     print("what is you name")
+
+# range(2)
+# range(start, stop, step)
+# for i in range(6):
+#     print(i)
+
+# names = ["Victor", "John", "Smith"]
+# # for i in range(3):
+# #     print(names[i].upper())
+# for name in names:
+#     print(name.upper())
+
+
+# WHILE LOOP
+# count = 0
+# while count < 10:
+#    print(count)
+#    count += 1
+
+# BREAK
+# for i in range(1, 10):
+#    if i % 2 == 0:
+#        break
+#    print(i)
+
+# Continue
+for i in range(1, 10):
+   if i % 2 == 0:
+       continue
+   print(i)
