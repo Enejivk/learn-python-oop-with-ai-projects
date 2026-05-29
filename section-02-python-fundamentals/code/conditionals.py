7f06f70271fb1527ae29b9830be1bab277dff108
enejivk <enejivic@gmail.com>
2026-05-29 03:29:23 +0100

    done with the conditional

---
+++ section-02-python-fundamentals/code/conditionals.py
@@ -0,0 +1,47 @@
+# CONDITIONALS
+# Conditional statements allow us to execute certain blocks of code based on specific conditions.
+
+# IF STATEMENT
+# The if statement is used to execute a block of code if a specified condition is true.
+
+# ELSE STATEMENT
+# The else statement is used to execute a block of code if the condition in the if statement is false.
+
+# ElIF STATEMENT
+# The elif statement is used to check multiple conditions. It stands for "else if"
+
+# budget = 100
+# if False:
+#     print("You have a high budget!")
+# else:
+#     print("You have a low budget!")
+
+
+# A is 90 above
+# B is 80 above
+# C is 70 above
+# D is 60 above
+# F is below 60
+# give the score print the grade of the student based on the above grading system.
+
+score = 85
+# if score >= 90:
+#     print("Grade: A")
+# elif score >= 80:
+#     print("Grade: B")
+# elif score >= 70:
+#     print("Grade: C")
+# elif score >= 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+if True:
+    print("This is coming from the if block.")
+elif True:
+    print("This is coming from the elif block.")
+else:
+    print("This will always be printed.")
+
+
+
